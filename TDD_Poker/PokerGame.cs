b1607e52a7b016a597e68ffd00a44352d934e27b
KuroKaraage <MantoFox@users.noreply.github.com>
2023-04-15 20:45:47 +0900

    [Test] R A01_BothHighCard

---
+++ TDD_Poker/PokerGame.cs
@@ -0,0 +1,9 @@
+namespace TDD_Poker;
+
+public class PokerGame
+{
+    public string ShowResult(string input)
+    {
+        throw new NotImplementedException();
+    }
+}
--- TDD_Poker/PokerGameTest.cs
+++ TDD_Poker/PokerGameTest.cs
@@ -1,16 +1,23 @@
+using FluentAssertions;
+
 namespace TDD_Poker
 {
     public class Tests
     {
+        private PokerGame _pokerGame;
+
         [SetUp]
-        public void Setup()
+        public void A00_Setup()
         {
+            _pokerGame = new PokerGame();
         }
 
         [Test]
-        public void Test1()
+        public void A01_BothHighCard()
         {
-            Assert.Pass();
+            var expected = "White wins. - with High card: Ace";
+            var actual = _pokerGame.ShowResult("Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH");
+            actual.Should().Be(expected);
         }
     }
 }
