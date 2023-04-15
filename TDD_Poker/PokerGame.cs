a7a688b265ac1e578b59a4ed4d6c72218e24f323
KuroKaraage <MantoFox@users.noreply.github.com>
2023-04-15 22:38:48 +0900

    [Test] G "Black: 2H 3D 5S 9C AD  White: 2C 3H 4S 8C AH", "Black wins. - with High card: 9"

--- TDD_Poker/PokerGame.cs
+++ TDD_Poker/PokerGame.cs
@@ -11,18 +11,34 @@ public class PokerGame
         var winnerName = "";
         var winnerOutput = "";
         var winnerPlayer = new Player();
+        var winnerCard = new Card();
+        var remainCard1 = players[0].Cards;
+        var remainCard2 = players[1].Cards;
+        var exceptCard1 = new List<Card>();
+        var exceptCard2 = new List<Card>();
 
+        if (compareResult == 0)
+        {
+            exceptCard1.Add(remainCard1.OrderByDescending(card => card.ActualValue).First());
+            remainCard1 = remainCard1.Except(exceptCard1).ToList();
+            exceptCard2.Add(remainCard2.OrderByDescending(card => card.ActualValue).First());
+            remainCard2 = remainCard2.Except(exceptCard2).ToList();
+            compareResult = remainCard1.Max(card => card.ActualValue) -
+                            remainCard2.Max(card => card.ActualValue);
+        }
         if (compareResult < 0)
         {
             winnerPlayer = players[1];
+            winnerCard = remainCard2.OrderByDescending(card => card.ActualValue).First();
         }
-        else if (compareResult > 0)
+
+        if (compareResult > 0)
         {
             winnerPlayer = players[0];
+            winnerCard = remainCard1.OrderByDescending(card => card.ActualValue).First();
         }
 
         winnerName = winnerPlayer.Name;
-        var winnerCard = winnerPlayer.Cards.OrderByDescending(card => card.ActualValue).First();
         winnerOutput = winnerCard.Output;
 
         return $"{winnerName} wins. - with High card: {winnerOutput}";
