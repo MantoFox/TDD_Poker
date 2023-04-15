168cfa1f7161c2eddb8e64cb2794e8cd414f7c57
KuroKaraage <MantoFox@users.noreply.github.com>
2023-04-15 22:21:31 +0900

    [Test] G "Black: 2H 3D 5S 9C JD  White: 2C 3H 4S 8C QH", "White wins. - with High card: Q"
    [Test] G "Black: 2H 3D 5S 9C TD  White: 2C 3H 4S 8C JH", "White wins. - with High card: J"
    [Test] R "Black: 2H 3D 5S 8C 9D  White: 2C 3H 4S 8C TH", "White wins. - with High card: 10"

--- TDD_Poker/PokerGameTest.cs
+++ TDD_Poker/PokerGameTest.cs
@@ -16,6 +16,9 @@ namespace TDD_Poker
         [TestCase("Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH", "White wins. - with High card: Ace")]
         [TestCase("Black: 2H 3D 5S 9C QD  White: 2C 3H 4S 8C KH", "White wins. - with High card: K")]
         [TestCase("Black: 2H 3D 5S 9C AD  White: 2C 3H 4S 8C 5H", "Black wins. - with High card: Ace")]
+        [TestCase("Black: 2H 3D 5S 9C JD  White: 2C 3H 4S 8C QH", "White wins. - with High card: Q")]
+        [TestCase("Black: 2H 3D 5S 9C TD  White: 2C 3H 4S 8C JH", "White wins. - with High card: J")]
+        [TestCase("Black: 2H 3D 5S 8C 9D  White: 2C 3H 4S 8C TH", "White wins. - with High card: 10")]
         public void A01_BothHighCard(string input, string expected)
         {
             AssertShowResultShouldBe(input, expected);
