1dc83adb604f419ce78c8d6f61cee2e764a086ab
KuroKaraage <MantoFox@users.noreply.github.com>
2023-04-15 21:19:29 +0900

    [Refactor] revise Parse

--- TDD_Poker/Parser.cs
+++ TDD_Poker/Parser.cs
@@ -4,28 +4,27 @@ public class Parser
 {
     public List<Player> Parse(string input)
     {
-        var playerSection = input.Split("  ", StringSplitOptions.RemoveEmptyEntries);
-        var playerName = playerSection[0].Split(":", StringSplitOptions.RemoveEmptyEntries)[0];
-        var cardSection = playerSection[0].Split(":", StringSplitOptions.RemoveEmptyEntries)[1];
-        var cards = cardSection.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-        var tempList = new List<Card>();
-
-        foreach (var card in cards)
-        {
-            tempList.Add(new Card
-            {
-                Output = card.Substring(0, 1),
-                Suit = card.Substring(1, 1)
-            });
-        }
+        var playerSection = input.Split("  ", StringSplitOptions.RemoveEmptyEntries)[0];
+        var player = GetPlayer(playerSection);
 
         return new List<Player>
         {
-            new Player
-            {
-                Name = playerName,
-                Cards = tempList
-            }
+            player
+        };
+    }
+
+    private static Player GetPlayer(string playerSection)
+    {
+        var playerName = playerSection.Split(":", StringSplitOptions.RemoveEmptyEntries)[0];
+        var cardSection = playerSection.Split(":", StringSplitOptions.RemoveEmptyEntries)[1];
+        var cards = cardSection.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(card => new Card { Output = card.Substring(0, 1), Suit = card.Substring(1, 1) })
+            .ToList();
+
+        return new Player
+        {
+            Name = playerName,
+            Cards = cards
         };
     }
 }
