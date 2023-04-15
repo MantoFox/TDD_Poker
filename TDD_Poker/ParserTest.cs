3557db93bd72c52546b4b740e42c14e57ab750e3
KuroKaraage <MantoFox@users.noreply.github.com>
2023-04-15 21:01:49 +0900

    [Test] R A02_TwoPlayer

--- TDD_Poker/ParserTest.cs
+++ TDD_Poker/ParserTest.cs
@@ -27,4 +27,40 @@ public class ParserTest
         };
         actual.Should().BeEquivalentTo(expected);
     }
+
+    [Test]
+    public void A02_TwoPlayer()
+    {
+        var parser = new Parser();
+        var actual = parser.Parse("Black: 2H 3D 5S 9C KD  White: 2C 3H 4S 8C AH");
+
+        var expected = new List<Player>
+        {
+            new Player
+            {
+                Name = "Black",
+                Cards = new List<Card>
+                {
+                    new Card{Output = "2", Suit = "H"},
+                    new Card{Output = "3", Suit = "D"},
+                    new Card{Output = "5", Suit = "S"},
+                    new Card{Output = "9", Suit = "C"},
+                    new Card{Output = "K", Suit = "D"}
+                }
+            },
+            new Player
+            {
+                Name = "White",
+                Cards = new List<Card>
+                {
+                    new Card{Output = "2", Suit = "C"},
+                    new Card{Output = "3", Suit = "H"},
+                    new Card{Output = "4", Suit = "S"},
+                    new Card{Output = "8", Suit = "C"},
+                    new Card{Output = "A", Suit = "H"}
+                }
+            }
+        };
+        actual.Should().BeEquivalentTo(expected);
+    }
 }
